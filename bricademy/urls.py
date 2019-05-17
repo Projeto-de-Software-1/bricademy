1f3f644a63b645ccf40f1a501a6a61f321d8b5fb
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-05-16 23:03:34 -0300

    adição template base, arquivos staticos e tentativa da home com materialize

--- bricademy/urls.py
+++ bricademy/urls.py
@@ -19,7 +19,7 @@ from .views import home
 from users import urls as users_urls
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
     path('users/', include(users_urls)),
     path('admin/', admin.site.urls),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import signup
 
+app_name = 'users'
 urlpatterns = [
-    path('cadastro', signup),
+    path('cadastro', signup, name="signup"),
 ]

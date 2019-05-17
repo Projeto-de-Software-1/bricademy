f7bc2d8cfc1c121e34e68869dbf479c8cf4104c3
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-05-17 18:27:30 -0300

    configurado mapa mostrando a localização do usuário em tempo real, apenas para teste

--- bricademy/views.py
+++ bricademy/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    # TODO
+    # tirar daqui e colocar ou no settings ou num .env
+    mapbox_access_token = "pk.eyJ1IjoiZG95bGVzbSIsImEiOiJjanZzZWx5bjgzNTE3M3lvajFxMmlyM3diIn0.RImU51Sa3cQqh50ZtCGBkA"
+    return render(request, 'home.html',  {'mapbox_access_token': mapbox_access_token})

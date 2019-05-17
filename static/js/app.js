3a3ae3d2a49501ecd471f15d577882404a4a6e59
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-05-17 13:43:23 -0300

    ajustes nos statics e scripts js

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+//Faz funcionar o click do navbar no mobile
+$(document).ready(function() {
+  $(".sidenav").sidenav();
+});
+
+//Faz funcionar o botão com menu na pagina home
+//só pra teste mesmo
+$(document).ready(function() {
+  $(".fixed-action-btn").floatingActionButton();
+});

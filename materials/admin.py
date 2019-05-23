3d300eca231ad3731fdf997a9a4a83f03b195bad
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-05-22 21:28:28 -0300

    classe material totalmente alterada, remover o banco de aplicar as migracoes novamente

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Material, Subject
 # Register your models here.
+admin.site.register(Material)
+admin.site.register(Subject)
--- materials/models.py
+++ materials/models.py
@@ -1,31 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Categories(models.Model):
-    category_name = models.CharField('Categoria', max_length=50)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.category_name
-
-
-class Conservations(models.Model):
-    CONSERVATION_CHOICES = [
-        ('NOVO', 0),
-        ('SEMINOVO', 1),
-        ('USADO', 2),
-        ('MUITO USADO', 3)
-    ]
-    conservation = models.IntegerField(choices=CONSERVATION_CHOICES)
+class Subject(models.Model):
+    subject = models.CharField('Assunto', max_length=100)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Modificado em', auto_now=True)
 
     def __str__(self):
-        return self.conservation
+        return self.subject
 
 
 class Material(models.Model):
+    CONSERVATION_CHOICES = [
+        (0, 'NOVO'),
+        (1, 'SEMINOVO'),
+        (2, 'USADO'),
+        (3, 'MUITO USADO')
+    ]
+    CATEGORY_CHOICES = [
+        (0, 'LIVRO'),
+        (1, 'TESE'),
+        (2, 'REVISTA'),
+        (3, 'ARTIGO'),
+        (4, 'RESUMO'),
+        (5, 'OUTRO')
+    ]
     title = models.CharField('Título', max_length=50)
     author = models.CharField('Autor', max_length=50)
-    ISBN = models.CharField('ISBN', max_length=13)
-    publishing_company = models.CharField('Editora', max_length=50)
+    ISBN = models.CharField('ISBN', max_length=13, blank=True, null=True)
+    publishing_company = models.CharField(
+        'Editora', max_length=50, blank=True, null=True)
     publication_date = models.DateField()
-    # falta
+    conservation = models.IntegerField(choices=CONSERVATION_CHOICES)
+    category = models.IntegerField(choices=CATEGORY_CHOICES)
+    subject = models.ManyToManyField(Subject)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Modificado em', auto_now=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+
+    # image = models.ImageField('Imagem', upload_to='materials', blank=True, null=True)
+
+    def __str__(self):
+        return self.title

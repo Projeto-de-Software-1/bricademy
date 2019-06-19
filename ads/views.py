15c36fded1565595167b6079f65bfd10c154985d
anaveroneze <ana_luisavs@hotmail.com>
2019-06-19 14:25:33 -0300

    Conf venda e home

--- ads/views.py
+++ ads/views.py
@@ -3,31 +3,35 @@ from materials.models import Ad, Material
 from django.http import HttpResponseNotFound
 from .forms import AdVendaForm
 
-
 def index(request):
     anuncios_venda = Ad.objects.filter(ad_type=0)
     materiais_venda = []
     for an in anuncios_venda:
         materiais_venda.append(an.material)
-    return render(request, 'ads/index.html', {'vendas': materiais_venda})
+    return render(request, 'home.html', {'vendas': materiais_venda})
 
+def venda(request, pk):
+    #Verifica se já existe um anúncio para aquele material. Se nao tiver anuncia, senão retorna direto.
+    ads = Ad.objects.filter(material=pk)
+    print(ads)
+    if not ads:
+        print("Material NAO anunciado")
+        material = get_object_or_404(Material, pk=pk)
+        if(material.user != request.user):
+            return HttpResponseNotFound("Este material não lhe pertence, voce não pode anunciar algo que não é seu, QUE PAPELÃO HEIN!")
+        if(request.method == 'GET'):
+            form = AdVendaForm()
+            return render(request, 'ads/new.html', {'form': form})
+        elif(request.method == 'POST'):
+            form = AdVendaForm(request.POST)
+            # form. = 0  # venda
+            if form.is_valid():
+                ad = form.save(commit=False)
+                ad.material = material
+                ad.ad_type = 0
+                ad.save()
+                error = False
+    else:
+        print("JA ANUNCIADO")
 
-def venda(request, pk=0):
-    # TODO
-    # verificar se ja existe algum tipo de anúcio pro material passado
-    material = get_object_or_404(Material, pk=pk)
-    if(material.user != request.user):
-        return HttpResponseNotFound("Este material não lhe pertence, voce não pode anunciar algo que não é seu, QUE PAPELÃO HEIN!")
-    if(request.method == 'GET'):
-        form = AdVendaForm()
-        return render(request, 'ads/new.html', {'form': form})
-    elif(request.method == 'POST'):
-        form = AdVendaForm(request.POST)
-        # form. = 0  # venda
-        if form.is_valid():
-            ad = form.save(commit=False)
-            ad.material = material
-            ad.ad_type = 0
-            ad.save()
-            error = False
-    return redirect('ads:index')
+    return redirect('home')
--- bricademy/views.py
+++ bricademy/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from materials.models import Ad, Material
+from django.http import HttpResponseNotFound
 
 def home(request):
-    return render(request, 'home.html')
+    anuncios_venda = Ad.objects.filter(ad_type=0)
+    materiais_venda = []
+    for an in anuncios_venda:
+        materiais_venda.append(an.material)
+    return render(request, 'home.html', {'vendas': materiais_venda})
+    # return render(request, 'home.html')

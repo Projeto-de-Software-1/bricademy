bc7fa78fef59603de8056981d3888732e44819d4
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-06-28 00:49:27 -0300

    adicao de mapa com listagem dos anuncios

--- bricademy/views.py
+++ bricademy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from materials.models import Ad, Material
 from django.http import HttpResponseNotFound
+from django.conf import settings
 
 
 def home(request):
@@ -14,3 +15,15 @@ def home(request):
     todos = list(zip(material, tipos))
     return render(request, 'home.html', {'materiais': todos})
     # return render(request, 'home.html')
+
+
+def homemap(request):
+    anuncios = Ad.objects.filter(deleted=0)
+    #material = []
+    #tipos = []
+    # for ad in anuncios:
+    #    if(ad.deleted == 0):
+    #        material.append(ad)
+    # tipos.append(ad.ad_type)
+    #todos = list(zip(material, tipos))
+    return render(request, 'ads/list_map.html', {'anuncios': anuncios, 'token': settings.MAPBOX_TOKEN})
